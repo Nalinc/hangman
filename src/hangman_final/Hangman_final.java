1a294ae77271c79c1fa905c1559b8ea5e8e133da
nalinc <nalin.chhibber@gmail.com>
2016-12-26 16:25:42 +0530

    project structural build

---
+++ src/hangman_final/Hangman_final.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman_final;
+
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Rachna Dutta
+ */
+public class Hangman_final {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    public static void main(String[] args) {
+
+    JFrame frame = new JFrame("HMFrame");
+    GamePanel Panel = new GamePanel();
+    //Panel.add
+    Panel.setVisible(true);
+    frame.setResizable(false);
+    frame.setSize(new Dimension(700, 500));
+//    frame.setContentPane(new JLabel(new javax.swing.ImageIcon("/bg.png")));
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(Panel);
+    frame.setVisible(true);        
+    }
+    
+}
